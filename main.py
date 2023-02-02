6d2bd7e65af62cd59920765c97161c96e5a5eae5
Kennedy Djeumi <114368365+Kennedydjeumi@users.noreply.github.com>
2023-02-02 23:40:21 +0000

    using the '|' symbol which acts as an or operator and enclosing the entire string in '^' and $ symbols I am able to limit the choice of flavors you're able to pick but very flexible with choices you're able to choose any choice

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
-import re 
-Upper=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-lower=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-symbols=["!", "@","#","$","%","^","&","*","(",")","_","+","-","=","{","}" ,]
-numbers=["1","2","3","4","5","6","7","8","9","0",]
- 
-all=Upper + lower + symbols 
+import re
 
-all_numbers=numbers
+print("Here is a list of chicken flavors choose which is your favorite:")
 
-password=input("Enter your password:")
+Flavors={
+  "Flavor 1":"Mango Habenara",
+  "Flavor 2":"Buffalo",
+  "Flavor 3":"BBQ",
+}
+print(Flavors)
 
-letter_count = len(re.findall(r"[a-zA-Z]", password))
+chicken_wing_flavors1=["BBQ","Buffalo","Mango Habenara"]
+while True:
+    flavor_Choice=input("Whats your favorite flavor: ")
 
-number_count = len(re.findall(r"\d", password))
-
-symbol_count = len(re.findall(r"[!@#\$%^&*()_\-+={}|;:<,>.?/]", password))
-
-if letter_count > 3:
-    print("Password should not contain more than 3 letters.")
-
-elif number_count > 3:
-
-  print("Password should not contain more than 3 numbers.")
-
-elif symbol_count > 3:
   
-  print("Password should not contain more than 3 symbols.")
 
-elif any(char not in all_chars for char in password):
+    joiner="^(" + "|".join(chicken_wing_flavors1)+")$"
 
-  print("Password should only contain letters, numbers, and symbols.")
+    match=re.match(joiner,flavor_Choice)
 
-else:
-    print("Password accepted.")
+    if match:
+      
+      print("Good choice")
+
+    else: 
+      
+      print("Try again ")
+      
+      satisfaction=input("Do you want to try again: (yes/no):")
+      
+      if satisfaction=="no":
+        
+        break 
+
+
+
+
+
+
+
+
+
